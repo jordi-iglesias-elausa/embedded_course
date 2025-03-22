1ccdd4b75c1516f423410a5c8899a60ac12e1424
Jordi Iglesias <jordimmg@gmail.com>
2025-03-22 10:55:12 +0000

    Deleted some unused files

--- .devcontainer/docker_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Get current directory from where the script is running
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#Update the git submodules
-git submodule update --init --recursive
-#Set executable permissions to all files within a relative path
-chmod -R +x ./tools/tasks
